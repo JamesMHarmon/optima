c2f2f65879a887813906af8246afb85269775cac
James Harmon <jamesmharmon@gmail.com>
2019-08-30 16:23:14 -0700

    Update quoridor engine with new wall placement

--- quoridor/src/engine.rs
+++ quoridor/src/engine.rs
@@ -6,16 +6,16 @@ const LEFT_COLUMN_MASK: u128 =                                  0b__100000000__1
 const RIGHT_COLUMN_MASK: u128 =                                 0b__000000001__000000001__000000001__000000001__000000001__000000001__000000001__000000001__000000001;
 
 const VALID_PIECE_POSITION_MASK: u128 =                         0b__111111111__111111111__111111111__111111111__111111111__111111111__111111111__111111111__111111111;
-const CANDIDATE_WALL_PLACEMENT_MASK: u128 =                     0b__000000000__011111111__011111111__011111111__011111111__011111111__011111111__011111111__011111111;
+const CANDIDATE_WALL_PLACEMENT_MASK: u128 =                     0b__000000000__111111110__111111110__111111110__111111110__111111110__111111110__111111110__111111110;
 const P1_OBJECTIVE_MASK: u128 =                                 0b__111111111__000000000__000000000__000000000__000000000__000000000__000000000__000000000__000000000;
 const P2_OBJECTIVE_MASK: u128 =                                 0b__000000000__000000000__000000000__000000000__000000000__000000000__000000000__000000000__111111111;
 const P1_STARTING_POS_MASK: u128 =                              0b__000000000__000000000__000000000__000000000__000000000__000000000__000000000__000000000__000010000;
 const P2_STARTING_POS_MASK: u128 =                              0b__000010000__000000000__000000000__000000000__000000000__000000000__000000000__000000000__000000000;
 
-const HORIZONTAL_WALL_LEFT_EDGE_TOUCHING_BOARD_MASK: u128 =     0b__000000000__010000000__010000000__010000000__010000000__010000000__010000000__010000000__010000000;
-const HORIZONTAL_WALL_RIGHT_EDGE_TOUCHING_BOARD_MASK: u128 =    0b__000000000__000000001__000000001__000000001__000000001__000000001__000000001__000000001__000000001;
-const VERTICAL_WALL_TOP_EDGE_TOUCHING_BOARD_MASK: u128 =        0b__000000000__011111111__000000000__000000000__000000000__000000000__000000000__000000000__000000000;
-const VERTICAL_WALL_BOTTOM_EDGE_TOUCHING_BOARD_MASK: u128 =     0b__000000000__000000000__000000000__000000000__000000000__000000000__000000000__000000000__011111111;
+const HORIZONTAL_WALL_LEFT_EDGE_TOUCHING_BOARD_MASK: u128 =     0b__000000000__100000000__100000000__100000000__100000000__100000000__100000000__100000000__100000000;
+const HORIZONTAL_WALL_RIGHT_EDGE_TOUCHING_BOARD_MASK: u128 =    0b__000000000__000000010__000000010__000000010__000000010__000000010__000000010__000000010__000000010;
+const VERTICAL_WALL_TOP_EDGE_TOUCHING_BOARD_MASK: u128 =        0b__000000000__111111110__000000000__000000000__000000000__000000000__000000000__000000000__000000000;
+const VERTICAL_WALL_BOTTOM_EDGE_TOUCHING_BOARD_MASK: u128 =     0b__000000000__000000000__000000000__000000000__000000000__000000000__000000000__000000000__111111110;
 
 #[derive(Hash, Eq, PartialEq, Clone, Debug)]
 pub struct GameState {
@@ -168,7 +168,7 @@ impl GameState {
     }
 
     fn get_horizontal_wall_blocks(&self) -> u128 {
-        self.horizontal_wall_placement_board << 1 | self.horizontal_wall_placement_board
+        self.horizontal_wall_placement_board >> 1 | self.horizontal_wall_placement_board
     }
 
     fn get_valid_horizontal_wall_placement(&self) -> u128 {
@@ -314,7 +314,7 @@ impl GameState {
     }
 
     fn get_move_right_mask(&self) -> u128 {
-        !self.get_vertical_wall_blocks() & VALID_PIECE_POSITION_MASK & !LEFT_COLUMN_MASK
+        !(self.get_vertical_wall_blocks() >> 1) & VALID_PIECE_POSITION_MASK & !LEFT_COLUMN_MASK
     }
 
     fn get_move_down_mask(&self) -> u128{
@@ -322,31 +322,31 @@ impl GameState {
     }
 
     fn get_move_left_mask(&self) -> u128 {
-        !(self.get_vertical_wall_blocks() << 1) & VALID_PIECE_POSITION_MASK & !RIGHT_COLUMN_MASK
+        !self.get_vertical_wall_blocks() & VALID_PIECE_POSITION_MASK & !RIGHT_COLUMN_MASK
     }
 
     fn get_horizontal_connecting_candidates(&self) -> u128 {
         let candidate_horizontal_walls = self.get_candidate_horizontal_wall_placement();
-        let horizontal_wall_blocks = self.get_horizontal_wall_blocks();
-        let vertical_wall_blocks = self.get_vertical_wall_blocks();
+        let horizontal_walls = self.horizontal_wall_placement_board;
+        let vertical_walls = self.vertical_wall_placement_board;
 
-        // Compare to existing walls. Wall segment candidates check against locations of possible new connections. These are checked in combinations since the walls may already connected. We don't want to count the same existing connection twice or thrice.
-        let middle_touching = candidate_horizontal_walls & (vertical_wall_blocks | vertical_wall_blocks >> 9);
-        let left_edge_touching = candidate_horizontal_walls & (vertical_wall_blocks >> 1 | vertical_wall_blocks >> 10 | horizontal_wall_blocks >> 2 | HORIZONTAL_WALL_LEFT_EDGE_TOUCHING_BOARD_MASK);
-        let right_edge_touching = candidate_horizontal_walls & (vertical_wall_blocks << 1 | vertical_wall_blocks >> 8 | horizontal_wall_blocks << 1 | HORIZONTAL_WALL_RIGHT_EDGE_TOUCHING_BOARD_MASK);
+        // Compare to existing walls. Wall segment candidates check against locations of possible new connections. These are checked in combinations since the walls may already be connected. We don't want to count the same existing connection twice or thrice.
+        let middle_touching = candidate_horizontal_walls & (vertical_walls << 9 | vertical_walls >> 9);
+        let left_edge_touching = candidate_horizontal_walls & (vertical_walls >> 1 | vertical_walls >> 10 | vertical_walls << 8 | horizontal_walls >> 2 | HORIZONTAL_WALL_LEFT_EDGE_TOUCHING_BOARD_MASK);
+        let right_edge_touching = candidate_horizontal_walls & (vertical_walls << 1 | vertical_walls << 10 | vertical_walls >> 8 | horizontal_walls << 2 | HORIZONTAL_WALL_RIGHT_EDGE_TOUCHING_BOARD_MASK);
 
         (left_edge_touching & middle_touching) | (left_edge_touching & right_edge_touching) | (middle_touching & right_edge_touching)
     }
 
     fn get_vertical_connecting_candidates(&self) -> u128 {
         let candidate_vertical_walls = self.get_candidate_vertical_wall_placement();
-        let vertical_wall_blocks = self.get_vertical_wall_blocks();
-        let horizontal_wall_blocks = self.get_horizontal_wall_blocks();
+        let vertical_walls = self.vertical_wall_placement_board;
+        let horizontal_walls = self.horizontal_wall_placement_board;
 
-        // Compare to existing walls. Wall segment candidates check against locations of possible new connections. These are checked in combinations since the walls may already connected. We don't want to count the same existing connection twice or thrice.
-        let middle_touching = candidate_vertical_walls & (horizontal_wall_blocks | horizontal_wall_blocks >> 1);
-        let top_edge_touching = candidate_vertical_walls & (horizontal_wall_blocks >> 9 | horizontal_wall_blocks >> 10 | vertical_wall_blocks >> 18 | VERTICAL_WALL_TOP_EDGE_TOUCHING_BOARD_MASK);
-        let bottom_edge_touching = candidate_vertical_walls & (horizontal_wall_blocks << 8 | horizontal_wall_blocks << 9 | vertical_wall_blocks << 9 | VERTICAL_WALL_BOTTOM_EDGE_TOUCHING_BOARD_MASK);
+        // Compare to existing walls. Wall segment candidates check against locations of possible new connections. These are checked in combinations since the walls may already be connected. We don't want to count the same existing connection twice or thrice.
+        let middle_touching = candidate_vertical_walls & (horizontal_walls >> 1 | horizontal_walls << 1);
+        let top_edge_touching = candidate_vertical_walls & (horizontal_walls >> 8 | horizontal_walls >> 9 | horizontal_walls >> 10 | vertical_walls >> 18 | VERTICAL_WALL_TOP_EDGE_TOUCHING_BOARD_MASK);
+        let bottom_edge_touching = candidate_vertical_walls & (horizontal_walls << 8 | horizontal_walls << 9 | horizontal_walls << 10 | vertical_walls << 18 | VERTICAL_WALL_BOTTOM_EDGE_TOUCHING_BOARD_MASK);
 
         (top_edge_touching & middle_touching) | (top_edge_touching & bottom_edge_touching) | (middle_touching & bottom_edge_touching)
     }
