6b3f4286d139543aaca82e9790f0c3eb117db657
James Harmon <jamesmharmon@gmail.com>
2019-09-11 21:39:30 -0700

    Change invalid puct scores error

--- mcts/src/mcts.rs
+++ mcts/src/mcts.rs
@@ -251,7 +251,7 @@ where
 
         let chosen_idx = match weighted_index {
             Err(_) => {
-                println!("Invalid puct scores. Most likely all are 0. Move will be randomly selected.");
+                // Invalid puct scores. Most likely all are 0. Move will be randomly selected.
                 rng.gen_range(0, pucts.len())
             },
             Ok(weighted_index) => weighted_index.sample(rng)
