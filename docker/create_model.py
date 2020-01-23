4664501691350101deaba3c97c35b589963d3b34
James Harmon <jamesmharmon@gmail.com>
2020-01-23 13:23:18 -0800

    Add moves_left size to model creation

--- docker/create_model.py
+++ docker/create_model.py
@@ -12,12 +12,13 @@ if __name__== "__main__":
     input_w             = int(os.environ['INPUT_W'])
     input_c             = int(os.environ['INPUT_C'])
     output_size         = int(os.environ['OUTPUT_SIZE'])
+    moves_left_size     = int(os.environ['MOVES_LEFT_SIZE'])
     num_filters         = int(os.environ['NUM_FILTERS'])
     num_blocks          = int(os.environ['NUM_BLOCKS'])
 
     c4.clear()
-    model = c4.create(num_filters, num_blocks, (input_h, input_w, input_c), output_size)
+    model = c4.create(num_filters, num_blocks, (input_h, input_w, input_c), output_size, moves_left_size)
 
     model.save(target_model_path)
 
-    c4.export(target_model_path, export_model_path, num_filters, num_blocks, (input_h, input_w, input_c), output_size)
+    c4.export(target_model_path, export_model_path, num_filters, num_blocks, (input_h, input_w, input_c), output_size, moves_left_size)
