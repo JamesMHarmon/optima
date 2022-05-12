a5a33b461225d32d62e390807e8e867bf42e9241
James Harmon <jamesmharmon@gmail.com>
2022-05-11 22:06:09 -0700

    Update network policy head

--- docker/model_sen.py
+++ docker/model_sen.py
@@ -7,6 +7,8 @@ from tensorflow.keras.optimizers import Nadam
 from tensorflow.keras import regularizers
 
 DATA_FORMAT = 'channels_last'
+ARIMAA_PIECE_MOVES = ['N','E','S','W','NN','NE','NW','EE','ES','SS','SW','WW','NNN','NNE','NNW','NEE','NWW','EEE','EES','ESS','SSS','SSW','SWW','WWW','NNNN','NNNE','NNNW','NNEE','NNWW','NEEE','NWWW','EEEE','EEES','EESS','ESSS','SSSS','SSSW','SSWW','SWWW','WWWW']
+ARIMAA_PUSH_PULL_MOVES = ['NN','NE','NW','EN','EE','ES','SE','SS','SW','WN','WS','WW']
 
 def l2_reg():
     return regularizers.l2(3e-5)
@@ -102,27 +104,27 @@ def PolicyHeadFullyConnected(x, filters, output_size):
 def ArimaaPolicyHeadConvolutional(x, filters, output_size):
     conv_block = ConvBlock(filters=filters, kernel_size=3, name='policy_head')(x)
 
-    def create_move_dir_out(cropping, name):
-        move_dir_conv = Conv2D(1, kernel_size=3, use_bias=True, bias_regularizer=l2_reg_policy(), kernel_regularizer=l2_reg_policy(), name=name)(conv_block)
-        move_dir_cropped = Cropping2D(cropping, data_format=DATA_FORMAT, name=name + '/cropping_2d')(move_dir_conv)
-        return Flatten()(move_dir_cropped)
+    def create_move_out(crop_dir, name):
+        cropping = ((crop_dir.count('N'), crop_dir.count('S')), (crop_dir.count('W'), crop_dir.count('E')))
+        move_conv = Conv2D(1, kernel_size=3, use_bias=True, bias_regularizer=l2_reg_policy(), kernel_regularizer=l2_reg_policy(), name=name)(conv_block)
+        move_cropped = Cropping2D(cropping, data_format=DATA_FORMAT, name=name + '/cropping_2d')(move_conv)
+        return Flatten()(move_cropped)
 
-    move_up_out = create_move_dir_out(cropping=((1, 0), (0, 0)), name='policy_head/up')
-    move_right_out = create_move_dir_out(cropping=((0, 0), (0, 1)), name='policy_head/right')
-    move_down_out = create_move_dir_out(cropping=((0, 1), (0, 0)), name='policy_head/down')
-    move_left_out = create_move_dir_out(cropping=((0, 0), (1, 0)), name='policy_head/left')
+    def create_piece_move_out(dir):
+        return create_move_out(crop_dir=dir, name='policy_head/move/' + dir)
+
+    def create_push_pull_out(dir):
+        crop_dir = dir[0] + dir[1].translate(str.maketrans('NESW', 'SWNE'))
+        return create_move_out(crop_dir=crop_dir, name='policy_head/push_pull/' + dir)
+
+    piece_move_outs = [create_piece_move_out(dir) for dir in ARIMAA_PIECE_MOVES]
+    push_pull_outs = [create_push_pull_out(dir) for dir in ARIMAA_PUSH_PULL_MOVES]
 
     pass_out = ConvBlock(filters=2, kernel_size=1, name='policy_head/pass')(conv_block)
     pass_out = Flatten()(pass_out)
     pass_out = Dense(1, activation=None, bias_regularizer=l2_reg_policy(), kernel_regularizer=l2_reg_policy(), name='policy_head/pass/2')(pass_out)
 
-    out = Concatenate(name='policy_head')([
-        move_up_out,
-        move_right_out,
-        move_down_out,
-        move_left_out,
-        pass_out
-    ])
+    out = Concatenate(name='policy_head')(piece_move_outs + push_pull_outs + [pass_out])
 
     return out
 
@@ -162,9 +164,9 @@ def create_model(num_filters, num_blocks, input_shape, output_size, moves_left_s
 
     value_head = ValueHead(net, num_filters)
 
-    if output_size == 225:
+    if output_size == 2245:
         policy_head = ArimaaPolicyHeadConvolutional(net, num_filters, output_size)
-    if output_size == 209:
+    elif output_size == 209:
         policy_head = QuoridorPolicyHeadConvolutional(net, num_filters, output_size)
     else:
         policy_head = PolicyHeadFullyConnected(net, num_filters, output_size)
